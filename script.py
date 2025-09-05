ee07f5ebf0a456ab07b8c5e9cecfd61de5c6f82f
veeravalli22 <vchaitanya20060@gmail.com>
2025-09-05 10:43:11 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,12 @@
 
+# Python Project
+
+This repository contains my first Python program for practice.  
+
+## File Description
+- *script.py* → A simple Python file that prints a message on the screen.
+
+## Features
+- Basic Python syntax
+- Beginner-friendly code
+- Easy to modify and expand
